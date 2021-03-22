8b30e68612295c21e2a35058bfdcc2669f0e1448
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-03-22 17:59:22 -0300

    add covid route
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- README.md
+++ README.md
@@ -36,10 +36,12 @@ chalice local --host 0.0.0.0
 
 Then you can:
 
-- Access `http://0.0.0.0:8000` on your navegator to get the `Hello World` Json maped on the app.py file.
+- Access `http://0.0.0.0:8000` on your navegator to get the `Hello World` JSON implemented on the `app.py` file.
 
 - Access `http://0.0.0.0:8000/time` on your navegator to get the current time.
 
 - POST a message on `http://0.0.0.0:8000/echo` to see the same message returned.
+
+- Access `http://0.0.0.0:8000/country/{country_name}` to get the covid-19 datas in JSON format from the informed country.
 
 - Access `http://0.0.0.0:8000/price/{symbol}` to get cryptocurrencies price values ([the full list of symbol can be obtained from here](https://coinmarketcap.com/api/documentation/v1/#section/Standards-and-Conventions)).
--- app.py
+++ app.py
@@ -15,19 +15,27 @@ def gettime():
     current_time = now.strftime("%D %H:%M:%S")
     return f"The time is {current_time}"
 
-
 @app.route('/echo', methods=['POST'])
 def echoback():
     request = app.current_request
     message = request.json_body
     return message
 
+@app.route('/covid/{country}')
+def index(country):
+    country = country.title()
+    url = f"https://covid-api.mmediagroup.fr/v1/cases?country={country}"
+    r = requests.get(url)
+    return r.content
 
 @app.route('/price/{symbol}')
 def index(symbol):
     symbol = symbol.upper()
     url = f"https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol={symbol}"
-    r = requests.get(url, headers={"X-CMC_PRO_API_KEY": API_KEY, "Content-Type":"application/json"})
+    r = requests.get(
+        url = url, 
+        headers={"X-CMC_PRO_API_KEY": API_KEY, "Content-Type":"application/json"}
+        )
     name = r.json()['data'][symbol][0]['name']
     price = r.json()['data'][symbol][0]['quote']['USD']['price']
     price = str('%.2f'%(float(price)))
